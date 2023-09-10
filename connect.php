b191bed871fed497afd45929513147bcf9736c5d
Ange Hervé <73267586+AngeHerve@users.noreply.github.com>
2023-09-10 10:49:17 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "brief5";
+$dbname = "spcom_kouassi";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
